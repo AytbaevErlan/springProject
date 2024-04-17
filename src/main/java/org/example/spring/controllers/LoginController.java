78816e4c04b1a631129ab902297e105cffc1c6dc
https://github.com/AytbaevErlan <irlanaytbaev@gmail.com>
2024-04-17 20:17:10 +0200

    web project

---
+++ src/main/java/org/example/spring/controllers/LoginController.java
@@ -0,0 +1,20 @@
+package org.example.spring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/test")
+public class LoginController {
+
+    @RequestMapping("/login_page")
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping("/login_remember_me")
+    public String loginRememberMe() {
+        return "login_remember_me";
+    }
+
+}
